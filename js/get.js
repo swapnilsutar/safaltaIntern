393faa1892b22755bf1232d9f1ea56a58615525a
swapnil sutar <swapnilsutar902@gmail.com>
2020-07-07 01:34:31 +0530

    Finish

--- js/get.js
+++ js/get.js
@@ -1,6 +1,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    
   }
   
 function closeNav() {
